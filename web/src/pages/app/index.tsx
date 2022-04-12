560e7fd8f0134be1f4300b0897d1d1a5a7b7bfdd
Matheus Chein <matheus.chein@fabric.inc>
2022-04-12 17:23:44 -0300

    aula 1 finalizada - configurando banco de dados, auth e graphql

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,37 @@
+import { getSession, useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { GetServerSideProps } from 'next';
+
+export default function Home() {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>Hello {user?.name}</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired();
+
+// isso aqui embaixo seria a forma manual de fazer o que o withPageAuthRequired faz
+
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   const session = getSession(req, res);
+
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/api/auth/login',
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {},
+//   };
+// };
