0867259f5876f198b42fc2143424f4440b225cf0
Matheus Chein <matheus.chein@fabric.inc>
2022-04-25 19:21:46 -0300

    aula 5 finalizada - front end realizando as queries e mutation que faltavam

---
+++ web/src/lib/withPublicApollo.tsx
@@ -0,0 +1,41 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  from,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import { GetServerSidePropsContext, NextPage } from 'next';
+
+export type ApolloClientContext = GetServerSidePropsContext;
+
+export function getApolloClient(
+  ctx?: ApolloClientContext,
+  ssrCache?: NormalizedCacheObject
+) {
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:3332/graphql',
+    fetch,
+  });
+
+  const cache = new InMemoryCache().restore(ssrCache ?? {});
+
+  const apolloClient = new ApolloClient({
+    link: from([httpLink]),
+    cache,
+  });
+
+  return apolloClient;
+}
+
+// Isso é um HOC - High Order Component
+export const withPublicApollo = (Component: NextPage) => {
+  return function Provider(props: any) {
+    return (
+      <ApolloProvider client={getApolloClient(undefined, props.apolloState)}>
+        <Component {...props} />
+      </ApolloProvider>
+    );
+  };
+};
